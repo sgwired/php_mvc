3b2db1a8d49461ec69fbf065d86b40d6d9353e60
Shelton Gayles <sgbythelake@gmail.com>
2017-06-27 00:04:37 -0700

    allow composer to autoload all my classes

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ require '../vendor/autoload.php';
 
 Twig_Autoloader::register();
 
-spl_autoload_register(function ($class){
-  $root = dirname(__DIR__);
-  $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-  if(is_readable($file)){
-    require $root . '/' . str_replace('\\', '/', $class) . '.php';
-  }
-});
+// spl_autoload_register(function ($class){
+//   $root = dirname(__DIR__);
+//   $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+//   if(is_readable($file)){
+//     require $root . '/' . str_replace('\\', '/', $class) . '.php';
+//   }
+// });
 
 $router = new Core\Router();
 
