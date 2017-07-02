b52b1cb6abb349eaa7763077fae5e1735e3f080c
Shelton Gayles <sgbythelake@gmail.com>
2017-07-01 17:24:30 -0700

    Added custom error pages and layout template

--- App/Config.php
+++ App/Config.php
@@ -8,4 +8,5 @@ class Config
   const DB_NAME = 'mvc';
   const DB_USER = 'root';
   const DB_PASSWORD = 'mysql';
+  const SHOW_ERRORS = false;
 }
--- Core/Router.php
+++ Core/Router.php
@@ -122,7 +122,7 @@ Class Router
       }
     } else {
       // echo "No route matched.";
-      throw new \Exception('No route matched.');
+      throw new \Exception('No route matched.', 404);
     }
   }
 
